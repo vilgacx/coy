6f95efd6c48e79e3935c86db79f5c7af2baf4058
xorvet <cypherx702@gmail.com>
2023-04-25 23:40:10 +0530

    instructions setup

--- src/views/InstructionView.vue
+++ src/views/InstructionView.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="instruct-body">
-    
+    <table class="instruct-list">
+      <tr>
+        <td class="tb-items">-</td>
+        <td class="tb-items">say ...</td>
+      </tr>
+      <tr>
+        <td class="tb-items">-</td>
+        <td class="tb-items">store ... in ...</td>
+      </tr>
+      <tr>
+        <td class="tb-items">-</td>
+        <td class="tb-items">if .. then .. else</td>
+      </tr>
+      <tr>
+        <td class="tb-items">-</td>
+        <td class="tb-items">repeat ... ... times</td>
+      </tr>
+      <tr>
+        <td class="tb-items">-</td>
+        <td class="tb-items">delay ... seconds</td>
+      </tr>
+      <tr>
+        <td class="tb-items">-</td>
+        <td class="tb-items">clear</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <style>
 .instruct-body {
-  @apply w-full h-full border-2;
+  @apply w-full h-full border-2 p-14;
+}
+
+.instruct-list {
+  @apply text-white text-xl;
+}
+
+.tb-items{
+  @apply p-4
 }
 </style>
--- src/views/PlaygroundView.vue
+++ src/views/PlaygroundView.vue
@@ -85,7 +85,7 @@ export default {
     },
     NewLine: function () {
       if (this.output !== "") {
-        return "\n"
+        return "\n";
       } else {
         return "";
       }

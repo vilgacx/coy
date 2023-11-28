2e21868d3634dee2fde4dc7a691388cea2cf62f1
xorvet <cypherx702@gmail.com>
2023-11-28 10:21:39 +0530

    update

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,13 +1,12 @@
 <main class="main home-body">
 	<div class="content-div">
 		<p class="title">COY</p>
-		<p class="sub">A minimal programming langauage simpler than python & lua.</p>
+		<p class="sub">A minimal web-based programming langauage written in typescript.</p>
 		<div class="links">
 			<a href="/play" use:link>playground</a>
-			<br>
 			<a href="/guide" use:link>how to use it?</a>
-      <br>
       <a href="https://github.com/vilgacx/coy/blob/main/README.md#disclaimer">disclaimer</a>
+      <a href="https://github.com/vilgacx/coy" use:link>source code</a>
 		</div>
 	</div> 
 </main>
@@ -25,11 +24,15 @@
 	@apply m-auto flex flex-col space-y-4;
 }
 
+.sub {
+  @apply md:w-96;
+}
+
 .title {
 	@apply text-9xl text-green-500 underline;
 }
 
 .links {
-	@apply decoration-2 underline underline-offset-4 text-blue-800;
+	@apply flex flex-col gap-1 decoration-2 underline underline-offset-4 text-blue-800;
 }
 </style>

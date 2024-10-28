1779dd59a61cf572ab6346592a79ab19043c8c04
vilgacx <cypherx702@gmail.com>
2024-10-28 15:44:43 +0530

    update

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ a toy programing languages.
 *Made with typescript, svelte + vite, svelte-spa-router & tailwindcss.*
 
 ### Disclaimer
-**I have zero clue about making a programming languages or compiler design – never learned any of that stuff. One morning I just woke up with a hackathon in 2 hours and no idea what to build. So I just thought I "let's make a programing language". I had keep it web based as cause it was a web-focused hackathon. I was given 6 hours and made it's initial version in those hours. I am continunig this after my hackathon cause why not. This project is more of a experiment.**
+**I had zero clue about making a programming languages or compiler design – never really learned any of that stuff at the time. One morning I just woke up with a hackathon in 2 hours and no idea what to build. So I just thought "I gotta make a programing language". I had to keep it web based cause it was a web-focused hackathon. I was given 6 hours and made it's initial version in those hours. I am continunig this after my hackathon cause why not. This project is more of a experiment.**
 
 ### How to use it ?
 Code it on: [coylang.netlify.app/#/play](https://coylang.netlify.app/#/play) <br/>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,38 +1,46 @@
+<script lang="ts">
+	import { link } from "svelte-spa-router";
+</script>
+
 <main class="main home-body">
 	<div class="content-div">
 		<p class="title">COY</p>
-		<p class="sub">A minimal web-based programming langauage written in typescript.</p>
+		<p class="sub">
+			A minimal web-based programming langauage written in
+			typescript.
+		</p>
 		<div class="links">
 			<a href="/play" use:link>playground</a>
 			<a href="/guide" use:link>how to use it?</a>
-      <a href="https://github.com/vilgacx/coy/blob/main/README.md#disclaimer">disclaimer</a>
-      <a href="https://github.com/vilgacx/coy" use:link>source code</a>
+			<a
+				href="https://github.com/vilgacx/coy/blob/main/README.md#disclaimer"
+				>disclaimer</a
+			>
+			<a href="https://github.com/vilgacx/coy" use:link
+				>source code</a
+			>
 		</div>
-	</div> 
+	</div>
 </main>
 
-<script>
-	import {link} from 'svelte-spa-router';
-</script>
-
 <style lang="postcss">
-.home-body {
-	@apply flex flex-col p-12;
-}
+	.home-body {
+		@apply flex flex-col p-12;
+	}
 
-.content-div {
-	@apply m-auto flex flex-col space-y-4;
-}
+	.content-div {
+		@apply m-auto flex flex-col space-y-4;
+	}
 
-.sub {
-  @apply md:w-96;
-}
+	.sub {
+		@apply md:w-96;
+	}
 
-.title {
-	@apply text-9xl text-green-500 underline;
-}
+	.title {
+		@apply text-9xl text-green-500 underline;
+	}
 
-.links {
-	@apply flex flex-col gap-1 decoration-2 underline underline-offset-4 text-blue-800;
-}
+	.links {
+		@apply flex flex-col gap-1 decoration-2 underline underline-offset-4 text-blue-800;
+	}
 </style>
